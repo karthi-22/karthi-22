83bb293bf8062a76acfafb952475b29154a091e7
i_karthikeyan.S <129253948+karthi-22@users.noreply.github.com>
2023-09-02 10:31:16 +0530

    Time: 212 ms (85.98%) | Memory: 16.4 MB (87.30%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,15 +1,12 @@
 class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        n=len(s)
-        dic = set(dictionary)
-        @cache
-        def dp(start):
-            if start == n:
-                return 0
-            ans = dp(start+1)+1
-            for end in range(start,n):
-                curr = s[start:end+1]
-                if curr in dic:
-                    ans = min(ans,dp(end+1))
-            return ans
-        return dp(0)
+    def minExtraChar(self, s: str, dic: List[str]) -> int:
+        n = len(s)
+        dic_set = set(dic)
+        dp = [0]*(n+1)
+        for i in range(n-1,-1,-1):
+            dp[i] = 1+dp[i+1]
+            for j in range(i,n):
+                curr = s[i:j+1]
+                if curr in dic_set:
+                    dp[i] = min(dp[i],dp[j+1])
+        return dp[0]
