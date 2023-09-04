c84b040ff9cf0aea7e3f53334b8152aa069ba38f
i_karthikeyan.S <129253948+karthi-22@users.noreply.github.com>
2023-09-04 09:57:21 +0530

    Time: 239 ms (78.94%) | Memory: 19.4 MB (7.25%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -1,11 +1,5 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        # Sort the list in ascending order
         nums.sort()
-        # Initialize sum to zero
-        max_sum = 0
-        for i in range(0, len(nums), 2):
-            # Add every element at even positions (0-indexed)
-            max_sum += nums[i]
-            
-        return max_sum
+        total_sum = sum(nums[::2])
+        return total_sum
